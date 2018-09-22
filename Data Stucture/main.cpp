6e6682baf0c73a83ab5e6998dbd1441a81703142
popshia <B123292179@gmail.com>
2018-09-22 11:35:38 +0800

    Update main.cpp
    峻瑋version, when N=2, got an memory error.

--- Data Stucture/main.cpp
+++ Data Stucture/main.cpp
@@ -39,7 +39,7 @@ void Perm( vector<int> dataBase, int digit, int functionN ) {
             Perm( Rotate( tempData, times, digit, functionN ) , digit + 1, functionN ) ;
         } // for()
     } // if()
-    
+
     else {
         Print( dataBase, functionN ) ;
     } // else()
@@ -57,39 +57,39 @@ void LesserThan() {
 void GivenN() {
     vector<int> dataBase ;
     int elements = 0 ;
-    
+
     for ( int i = 0 ; i < TwoN ; i ++ ) {
         cin >> elements ;
         dataBase.push_back( elements ) ;
     } // for()
-    
+
     Perm( dataBase, 0, TwoN) ;
 } // GivenN()
 
 void GivenLength( vector<int> & dataBase, int layer ) {
     if ( layer != ThreeN - 1 ) {
         while ( dataBase[0] <= 10 - ThreeN ) {
-            cout << dataBase[layer] << " " << dataBase[layer+1] << endl ;
-            if ( jump == true || dataBase[layer+1] == 9 ) dataBase[layer] ++ ;
-            
-            for ( int time = layer ; time < ThreeN ; time ++ ) {
-                dataBase[time+1] = dataBase[time] + 1 ;
-            } // for()
-                
-            jump = false ;
-            GivenLength( dataBase, layer + 1 ) ;
-   
-            if ( dataBase[layer] == dataBase[layer+1] - 1 ) {
-                layer -- ;
-                jump = true ;
-                // cout << "jump backwards" << endl ;
-            } // if()
+
+              while ( dataBase[layer] > layer + 10- ThreeN && layer > 0 ) { // 位數往上
+                layer--;
+                dataBase[layer]++;
+                for ( int time = layer ; time < ThreeN ; time ++ )
+                  dataBase[time+1] = dataBase[time] + 1 ;
+                  cout << "layer:" << layer << endl;
+              } // while
+
+              GivenLength( dataBase, layer + 1 ) ;
+
+              dataBase[layer]++;
+              for ( int time = layer ; time < ThreeN ; time ++ )
+                  dataBase[time+1] = dataBase[time] + 1 ;
+
         } // while()
     } // if()
-    
+
     else if( layer == ThreeN - 1 ) {
         Perm( dataBase, 0, ThreeN) ;
-        
+
         while ( dataBase[layer] < 9 ) {
             dataBase[layer] ++ ;
             Perm( dataBase, 0, ThreeN) ;
@@ -100,7 +100,7 @@ void GivenLength( vector<int> & dataBase, int layer ) {
 int main( int argc, const char * argv[] ) {
     int command = 0 ;
     char continueOrNot = '\0' ;
-    
+
     cout << "*******************************************" << endl ;
     cout << "*******     Permutation Practice      *****" << endl ;
     cout << "******* 0 : Quit                      *****" << endl ;
@@ -108,31 +108,31 @@ int main( int argc, const char * argv[] ) {
     cout << "******* 2 : N numbers from user input *****" << endl ;
     cout << "******* 3 : N numbers from 1 ... 9    *****" << endl ;
     cout << "*******************************************" << endl ;
-    
+
     do {
-        
+
         cout << endl << "Please enter your choice :" << endl ;
         cin >> command ;
-        
+
         while ( command > 3 || command < 0 ) {
             cout << "Error command! please enter an acceptable command :" << endl ;
             cin >> command ;
         } // while()
-        
+
         if ( command == 0 ) {
             cout << "Bye :((((" << endl ;
             return 0 ;
         } // if()
-        
+
         if ( command == 1 ) {
             cout << "Please enter your N ( 1~9 ) :" << endl ;
             cin >> OneN ;
-            
+
             while ( OneN <= 0 || OneN > 9 ) {
                 cout << "Error N! please enter an acceptable N :" << endl ;
                 cin >> OneN ;
             } // while()
-            
+
             clock_t time ;
             time = clock() ;
             LesserThan() ;
@@ -142,12 +142,12 @@ int main( int argc, const char * argv[] ) {
         else if ( command == 2 ) {
             cout << "Please enter your N ( 1~9 ) :" << endl ;
             cin >> TwoN ;
-            
+
             while ( TwoN <= 0 || TwoN > 9 ) {
                 cout << "Error N! please enter an acceptable N :" << endl ;
                 cin >> TwoN ;
             } // while()
-            
+
             clock_t time ;
             time = clock() ;
             GivenN() ;
@@ -157,29 +157,29 @@ int main( int argc, const char * argv[] ) {
         else if ( command == 3 ) {
             cout << "Please enter your N ( 1~9 ) :" << endl ;
             cin >> ThreeN ;
-            
+
             while ( ThreeN <= 0 || ThreeN > 9 ) {
                 cout << "Error N! please enter an acceptable N :" << endl ;
                 cin >> ThreeN ;
             } // while()
-            
+
             vector<int> dataBase;
             for ( int i = 1 ; i <= ThreeN ; i ++ ) dataBase.push_back(i) ;
             GivenLength( dataBase, 0 ) ;
         } // if()
-        
+
         cout << "Do you want to practice again? (Y/N)" << endl ;
         cin >> continueOrNot ;
-        
+
         while( continueOrNot != 'Y' && continueOrNot != 'y' && continueOrNot != 'N' && continueOrNot != 'n' ) {
             cout << "Error command! please enter an acceptable command :" << endl ;
             cin >> continueOrNot ;
         } // while()
-        
+
         Count = 0 ;
         Number = 1 ;
-        
+
     } while( continueOrNot == 'Y' || continueOrNot == 'y' ) ;
-    
+
     cout << "Bye :(((" ;
 }
